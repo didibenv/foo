61e5c85b7b1b9cf06050dde86706bb982437c409
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-06 20:41:39 -0200

    Fix clang on linux ci

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo -E apt-get update -qq
+  if [ "$COMPILER" == "clang++" ]; then
+    sudo apt-get -y install libc++-dev
+  fi
 fi
--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -3,7 +3,13 @@
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   mkdir build
   cd build
-  cmake -DCMAKE_CXX_COMPILER=$COMPILER ..
+
+  if [ "$COMPILER" == "clang++" ]; then
+    cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS="-Wall -Wextra -stdlib=libc++ -std=c++14" ..
+  else
+    cmake -DCMAKE_CXX_COMPILER=$COMPILER ..
+  fi
+
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
   mkdir build
   cd build
