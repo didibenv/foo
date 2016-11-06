9327d30b052ac8703748ed6b0a1385670c3b0b8a
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-06 18:54:22 -0200

    Update Travis ci scripts for osx

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -4,6 +4,10 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   mkdir build
   cd build
   cmake -DCMAKE_CXX_COMPILER=$COMPILER ..
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  mkdir build
+  cd build
+  cmake ..
 fi
 
 make
